09e6ddfd1c5b7f44be55a75237ece28bb1d244cf
Jiyun Kwon <20087623@mail.wit.ie>
2024-10-31 09:34:33 +0000

    Add to favourites logic completed; Dummy favourites page.

--- movies/src/pages/homePage.js
+++ movies/src/pages/homePage.js
@@ -22,6 +22,12 @@ const HomePage = (props) => {
     if (type === "name") setNameFilter(value);
     else setGenreFilter(value);
   };
+  const addToFavorites = (movieId) => {
+    const updatedMovies = movies.map((m) =>
+      m.id === movieId ? { ...m, favorite: true } : m
+    );
+    setMovies(updatedMovies);
+  };
   useEffect(() => {
     fetch(
       `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
@@ -49,8 +55,8 @@ const HomePage = (props) => {
       genreFilter={genreFilter}
     />
         </Grid>
-        <MovieList movies={displayedMovies} />
-      </Grid>
+        <MovieList movies={displayedMovies} selectFavorite={addToFavorites} />
+        </Grid>
     </Grid>
   );
 };
