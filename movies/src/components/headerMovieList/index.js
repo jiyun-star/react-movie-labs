005aded57be0685c5125865251f36a2975d6cddc
Jiyun Kwon <20087623@mail.wit.ie>
2024-10-31 11:46:38 +0000

    Using react-query to manage the browser-based cache.

--- movies/src/components/headerMovieList/index.js
+++ movies/src/components/headerMovieList/index.js
@@ -6,6 +6,7 @@ import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import { useNavigate } from "react-router-dom";
 
+
 const Header = (props ) => {
   const title = props.title
   const navigate = useNavigate();
