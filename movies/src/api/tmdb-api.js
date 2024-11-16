9fba16c9fd302f17bd2b524a57c05fd5a112adf7
Jiyun Kwon <20087623@mail.wit.ie>
2024-11-16 12:51:29 +0000

    made top-rated page

--- movies/src/api/tmdb-api.js
+++ movies/src/api/tmdb-api.js
@@ -14,6 +14,22 @@ export const getUpcoming = () => {
   });
 };
 
+export const toprated = () => {
+  return fetch(
+    `https://api.themoviedb.org/3/discover/top-rated?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
+  ).then((response) => {
+    if (!response.ok) {
+      return response.json().then((error) => {
+        throw new Error(error.status_message || "Something went wrong");
+      });
+    }
+    return response.json();
+  })
+  .catch((error) => {
+      throw error
+  });
+};
+
 export const getMovies = () => {
   return fetch(
     `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
